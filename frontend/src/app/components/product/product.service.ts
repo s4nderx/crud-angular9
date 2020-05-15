534ba78a8983b341c9010c818209ca8485cdcf2b
Phellipe Sander <phellipe.sander1@gmail.com>
2020-05-15 01:33:02 -0300

    adicionando funcao de delete no serviceProduct

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -12,8 +12,8 @@ export class ProductService {
   baseUrl = "http://localhost:3001/products"
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
-  
-  showMessage(msg: string): void{
+
+  showMessage(msg: string): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: "right",
@@ -21,22 +21,27 @@ export class ProductService {
     })
   }
 
-  create(product: Product): Observable<Product>{
+  create(product: Product): Observable<Product> {
     return this.http.post<Product>(this.baseUrl, product)
   }
 
-  read():Observable<Product[]>{
+  read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl)
   }
 
-  readById(id: string): Observable<Product>{
-      const url = `${this.baseUrl}/${id}`
-      return this.http.get<Product>(url)
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
   }
 
-  update(product: Product): Observable<Product>{
+  update(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`
     return this.http.put<Product>(url, product)
+  }
+
+  delete(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<Product>(url)
   }
 
 }
